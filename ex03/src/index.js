1e1e33348a29dc07227f5cdd7a1917e97ac03cbf
nejira-k <kadric.nejira@gmail.com>
2021-03-12 22:35:52 +0100

    Added new files

---
+++ ex03/src/index.js
@@ -0,0 +1,8 @@
+function main() {
+
+    var myString = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+    return myString;
+}
+console.log(main());
+module.exports = main;
---
+++ ex06/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+
+    var myName = "Liz";
+    var myString = "My names is " + myName + " and I am well!";
+
+    return myString;
+}
+console.log(main());
+module.exports = main;
